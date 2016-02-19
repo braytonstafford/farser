aeae2ee26987ba8f8ce86ffd13c6ddc4f42a86e5
Brayton Stafford <brayton82@gmail.com>
2016-02-19 08:52:57 -0500

    Add google analytics

--- public/js/app.js
+++ public/js/app.js
@@ -51,10 +51,8 @@ config(function($mdThemingProvider) {
 }).
 run(['$rootScope', '$location', '$window', function($rootScope, $location, $window) {
   $rootScope
-    .$on('$stateChangeSuccess',
+    .$on('$routeChangeSuccess',
       function(event){
-        if (!$window.ga)
-          return;
         $window.ga('send', 'pageview', { page: $location.path() });
       });
 }]);
